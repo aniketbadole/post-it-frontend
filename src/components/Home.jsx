19006eef59878a70e02947f9e35f6838a36780e6
aniket.b <aniket.b@outlook.com>
2023-08-27 14:24:09 +0530

    fixing minor bugs

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -38,17 +38,17 @@ const Home = () => {
     <div>
       <Header />
       <div className="flex gap-4">
-        <div className="w-1/5 h-32 rounded-lg bg-gray-100">
+        <div className="w-1/5 rounded-lg bg-gray-100">
           {/* <p>{userData}</p>
           <p>{user._id}</p> */}
           <SideBar />
         </div>
-        <div className="w-3/5 break-all">
+        <div className="w-3/5 mt-4 break-all">
           <TweetForm />
           {condition ? <p>present</p> : history("/login")}
           <p>{localStorage.getItem("token")}</p>
         </div>
-        <div className="w-1/5 h-32 rounded-lg bg-gray-100"></div>
+        <div className="w-1/5 rounded-lg bg-gray-100"></div>
       </div>
     </div>
   );
