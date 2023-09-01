72686851884601ab4de3138a1b42b4ccb9f1a5df
aniket.b <aniket.b@outlook.com>
2023-09-01 20:32:51 +0530

    code simplified, minor changes

--- src/components/Tweet/GetTimeline.jsx
+++ src/components/Tweet/GetTimeline.jsx
@@ -4,10 +4,11 @@ import TweetCard from "./TweetCard";
 
 const GetTimeline = () => {
   const [cachedTweets, setCachedTweets] = useState([]);
+  const [showNotification, setShowNotification] = useState(false);
 
   const fetchTweets = async () => {
     try {
-      const response = await apiService.getTimeline(); // Adjust the API call accordingly
+      const response = await apiService.getTimeline();
       setCachedTweets(response.data);
     } catch (error) {
       console.error("Error fetching tweets:", error);
@@ -19,7 +20,7 @@ const GetTimeline = () => {
     fetchTweets();
 
     // Polling interval every 15 seconds
-    const intervalId = setInterval(fetchTweets, 20000);
+    const intervalId = setInterval(fetchTweets, 2000);
 
     // Clean up the interval when the component unmounts
     return () => {
@@ -30,10 +31,40 @@ const GetTimeline = () => {
   // Memoize the cachedTweets array
   const tweets = useMemo(() => cachedTweets, [cachedTweets]);
 
+  const handleDelete = (tweetId) => {
+    // Delete the tweet from the list
+    const updatedTweets = cachedTweets.filter((tweet) => tweet._id !== tweetId);
+    setCachedTweets(updatedTweets);
+
+    // Call your API to delete the tweet from the server
+    apiService
+      .deleteTweet(tweetId)
+      .then(() => {
+        console.log("Tweet deleted successfully");
+        setShowNotification(true);
+        setTimeout(() => {
+          setShowNotification(false); // Hide the notification after a short delay
+        }, 3000);
+      })
+      .catch((error) => {
+        console.error("Error deleting tweet", error);
+      });
+  };
+
   return (
     <div className="get-timeline">
-      {console.log(tweets)}
-      <TweetCard tweets={tweets} /> {/* Pass tweets to TweetCard */}
+      {showNotification && (
+        <div className="fixed inset-x-0 bottom-0">
+          <div className="bg-indigo-600 px-4 py-3 text-white">
+            <p className="text-center text-sm font-medium">
+              Message deleted successfully!
+            </p>
+          </div>
+        </div>
+      )}
+      {tweets.map((tweet) => (
+        <TweetCard key={tweet._id} tweet={tweet} onDelete={handleDelete} />
+      ))}
     </div>
   );
 };
--- src/components/Tweet/TweetCard.jsx
+++ src/components/Tweet/TweetCard.jsx
@@ -1,144 +1,131 @@
 import jwtDecode from "jwt-decode";
-import apiService from "../services/apiService";
 import PropTypes from "prop-types";
 
 const token = localStorage.getItem("token");
 const decodedToken = jwtDecode(token);
 const userId = decodedToken.user.id;
 
-const TweetCard = ({ tweets }) => {
-  const handleDelete = (tweetId) => {
-    apiService
-      .deleteTweet(tweetId)
-      .then(() => {
-        console.log("Tweet deleted successfully");
-      })
-      .catch((error) => {
-        console.error("Error deleting tweet", error);
-      });
+const TweetCard = ({ tweet, onDelete }) => {
+  const handleDelete = () => {
+    onDelete(tweet._id);
   };
 
   return (
     <div>
-      {tweets.map((tweet) => (
-        <div key={tweet._id} className="tweet">
-          <article className="rounded-xl border-2 border-gray-100 bg-white">
-            <div className="flex items-start gap-4 p-4 sm:p-6 lg:p-8">
-              <a href="#" className="block shrink-0">
-                <img
-                  alt="Speaker"
-                  src="https://images.unsplash.com/photo-1570295999919-56ceb5ecca61?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxzZWFyY2h8NHx8YXZhdGFyfGVufDB8fDB8fA%3D%3D&auto=format&fit=crop&w=800&q=60"
-                  className="h-14 w-14 rounded-lg object-cover"
-                />
-              </a>
+      <div key={tweet._id} className="tweet">
+        <article className="rounded-xl border-2 border-gray-100 bg-white">
+          <div className="flex items-start gap-4 p-4 sm:p-6 lg:p-8">
+            <a href="#" className="block shrink-0">
+              <img
+                alt="Speaker"
+                src="https://images.unsplash.com/photo-1570295999919-56ceb5ecca61?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxzZWFyY2h8NHx8YXZhdGFyfGVufDB8fDB8fA%3D%3D&auto=format&fit=crop&w=800&q=60"
+                className="h-14 w-14 rounded-lg object-cover"
+              />
+            </a>
 
-              <div>
-                <h3 className="font-medium sm:text-lg">
-                  <a href="#" className="hover:underline">
-                    {tweet.author.name}
-                  </a>
-                </h3>
+            <div>
+              <h3 className="font-medium sm:text-lg">
+                <a href="#" className="hover:underline">
+                  {tweet.author.name}
+                </a>
+              </h3>
+
+              <p className="hidden sm:block sm:text-xs sm:text-gray-500">
+                @{tweet.author.username}
+              </p>
+
+              <p className="mt-2 line-clamp-2 text-sm text-gray-700">
+                {tweet.content}
+              </p>
+
+              <div className="mt-2 sm:flex sm:items-center sm:gap-2">
+                <div className="flex items-center gap-1 text-gray-500">
+                  <svg
+                    xmlns="http://www.w3.org/2000/svg"
+                    className="h-4 w-4"
+                    fill="none"
+                    viewBox="0 0 24 24"
+                    stroke="currentColor"
+                    strokeWidth="2"
+                  >
+                    <path
+                      strokeLinecap="round"
+                      strokeLinejoin="round"
+                      d="M17 8h2a2 2 0 012 2v6a2 2 0 01-2 2h-2v4l-4-4H9a1.994 1.994 0 01-1.414-.586m0 0L11 14h4a2 2 0 002-2V6a2 2 0 00-2-2H5a2 2 0 00-2 2v6a2 2 0 002 2h2v4l.586-.586z"
+                    />
+                  </svg>
+
+                  <p className="text-xs">{tweet.mentions.length} replies</p>
+                </div>
+
+                <span className="hidden sm:block" aria-hidden="true">
+                  &middot;
+                </span>
+
+                <div className="flex items-center gap-1 text-gray-500">
+                  <svg
+                    xmlns="http://www.w3.org/2000/svg"
+                    className="h-4 w-4"
+                    fill="none"
+                    viewBox="0 0 24 24"
+                    stroke="currentColor"
+                    strokeWidth="2"
+                  >
+                    <path
+                      strokeLinecap="round"
+                      strokeLinejoin="round"
+                      d="M17 8h2a2 2 0 012 2v6a2 2 0 01-2 2h-2v4l-4-4H9a1.994 1.994 0 01-1.414-.586m0 0L11 14h4a2 2 0 002-2V6a2 2 0 00-2-2H5a2 2 0 00-2 2v6a2 2 0 002 2h2v4l.586-.586z"
+                    />
+                  </svg>
+
+                  <p className="text-xs">{tweet.retweets.length} reposts</p>
+                </div>
+
+                <span className="hidden sm:block" aria-hidden="true">
+                  &middot;
+                </span>
+
+                <div className="flex items-center gap-1 text-gray-500">
+                  <svg
+                    xmlns="http://www.w3.org/2000/svg"
+                    className="h-4 w-4"
+                    fill="none"
+                    viewBox="0 0 24 24"
+                    stroke="currentColor"
+                    strokeWidth="2"
+                  >
+                    <path
+                      strokeLinecap="round"
+                      strokeLinejoin="round"
+                      d="M17 8h2a2 2 0 012 2v6a2 2 0 01-2 2h-2v4l-4-4H9a1.994 1.994 0 01-1.414-.586m0 0L11 14h4a2 2 0 002-2V6a2 2 0 00-2-2H5a2 2 0 00-2 2v6a2 2 0 002 2h2v4l.586-.586z"
+                    />
+                  </svg>
+
+                  <p className="text-xs">{tweet.likes.length} likes</p>
+                </div>
+
+                <span className="hidden sm:block" aria-hidden="true">
+                  &middot;
+                </span>
 
                 <p className="hidden sm:block sm:text-xs sm:text-gray-500">
-                  @{tweet.author.username}
+                  Posted {formatTimestamp(tweet.createdAt)}
                 </p>
-
-                <p className="mt-2 line-clamp-2 text-sm text-gray-700">
-                  {tweet.content}
-                </p>
-
-                <div className="mt-2 sm:flex sm:items-center sm:gap-2">
-                  <div className="flex items-center gap-1 text-gray-500">
-                    <svg
-                      xmlns="http://www.w3.org/2000/svg"
-                      className="h-4 w-4"
-                      fill="none"
-                      viewBox="0 0 24 24"
-                      stroke="currentColor"
-                      strokeWidth="2"
-                    >
-                      <path
-                        strokeLinecap="round"
-                        strokeLinejoin="round"
-                        d="M17 8h2a2 2 0 012 2v6a2 2 0 01-2 2h-2v4l-4-4H9a1.994 1.994 0 01-1.414-.586m0 0L11 14h4a2 2 0 002-2V6a2 2 0 00-2-2H5a2 2 0 00-2 2v6a2 2 0 002 2h2v4l.586-.586z"
-                      />
-                    </svg>
-
-                    <p className="text-xs">{tweet.mentions.length} replies</p>
-                  </div>
-
-                  <span className="hidden sm:block" aria-hidden="true">
-                    &middot;
-                  </span>
-
-                  <div className="flex items-center gap-1 text-gray-500">
-                    <svg
-                      xmlns="http://www.w3.org/2000/svg"
-                      className="h-4 w-4"
-                      fill="none"
-                      viewBox="0 0 24 24"
-                      stroke="currentColor"
-                      strokeWidth="2"
-                    >
-                      <path
-                        strokeLinecap="round"
-                        strokeLinejoin="round"
-                        d="M17 8h2a2 2 0 012 2v6a2 2 0 01-2 2h-2v4l-4-4H9a1.994 1.994 0 01-1.414-.586m0 0L11 14h4a2 2 0 002-2V6a2 2 0 00-2-2H5a2 2 0 00-2 2v6a2 2 0 002 2h2v4l.586-.586z"
-                      />
-                    </svg>
-
-                    <p className="text-xs">{tweet.retweets.length} reposts</p>
-                  </div>
-
-                  <span className="hidden sm:block" aria-hidden="true">
-                    &middot;
-                  </span>
-
-                  <div className="flex items-center gap-1 text-gray-500">
-                    <svg
-                      xmlns="http://www.w3.org/2000/svg"
-                      className="h-4 w-4"
-                      fill="none"
-                      viewBox="0 0 24 24"
-                      stroke="currentColor"
-                      strokeWidth="2"
-                    >
-                      <path
-                        strokeLinecap="round"
-                        strokeLinejoin="round"
-                        d="M17 8h2a2 2 0 012 2v6a2 2 0 01-2 2h-2v4l-4-4H9a1.994 1.994 0 01-1.414-.586m0 0L11 14h4a2 2 0 002-2V6a2 2 0 00-2-2H5a2 2 0 00-2 2v6a2 2 0 002 2h2v4l.586-.586z"
-                      />
-                    </svg>
-
-                    <p className="text-xs">{tweet.likes.length} likes</p>
-                  </div>
-
-                  <span className="hidden sm:block" aria-hidden="true">
-                    &middot;
-                  </span>
-
-                  <p className="hidden sm:block sm:text-xs sm:text-gray-500">
-                    Posted {formatTimestamp(tweet.createdAt)}
-                  </p>
-                  <span className="hidden sm:block" aria-hidden="true">
-                    &middot;
-                  </span>
-                  <div className="flex items-center gap-1 text-gray-500">
-                    {tweet.author._id === userId && (
-                      <button
-                        className="text-xs"
-                        onClick={() => handleDelete(tweet._id)}
-                      >
-                        Delete
-                      </button>
-                    )}
-                  </div>
+                <span className="hidden sm:block" aria-hidden="true">
+                  &middot;
+                </span>
+                <div className="flex items-center gap-1 text-gray-500">
+                  {tweet.author._id === userId && (
+                    <button className="text-xs" onClick={handleDelete}>
+                      Delete
+                    </button>
+                  )}
                 </div>
               </div>
             </div>
-          </article>
-        </div>
-      ))}
+          </div>
+        </article>
+      </div>
     </div>
   );
 };
@@ -167,7 +154,8 @@ const formatTimestamp = (timestamp) => {
 };
 
 TweetCard.propTypes = {
-  tweets: PropTypes.array,
+  tweet: PropTypes.array,
+  onDelete: PropTypes.func,
 };
 
 export default TweetCard;
